2355d1a0afcf6302dbe244b6cb292093130475b3
JolyneFr <jolynefr@sjtu.edu.cn>
2021-07-19 09:35:16 +0800

    init commit

---
+++ src/test/java/com/privateboat/forum/backend/BusinessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.privateboat.forum.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
