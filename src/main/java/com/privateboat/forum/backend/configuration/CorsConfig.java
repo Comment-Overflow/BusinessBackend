22bed7a060ca4242fc1c3736709867f098e2f54f
Gun9niR <gun9nir.guo@gmail.com>
2021-07-21 00:06:51 +0800

    feat: add CORS filter

---
+++ src/main/java/com/privateboat/forum/backend/configuration/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.privateboat.forum.backend.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("**")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
+
