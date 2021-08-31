679db7e090271e1c036331c789c83be0c90f20cd
Gun9niR <gun9nir.guo@gmail.com>
2021-09-01 01:35:29 +0800

    optiimize: use native query with @Modifying annotation

--- src/main/java/com/privateboat/forum/backend/dao/UserStatisticDAO.java
+++ src/main/java/com/privateboat/forum/backend/dao/UserStatisticDAO.java
@@ -2,17 +2,17 @@ package com.privateboat.forum.backend.dao;
 
 import com.privateboat.forum.backend.entity.UserStatistic;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-
-import javax.persistence.LockModeType;
 
 public interface UserStatisticDAO extends JpaRepository<UserStatistic, Long> {
     UserStatistic.NewRecord getNewlyRecordByUserId(Long userId);
 
+    @Modifying
     @Query(value = "update user_statistic set approval_count = approval_count + 1 where user_id = ?1", nativeQuery = true)
     void incrementApprovalCountByUserId(Long userId);
 
+    @Modifying
     @Query(value = "update user_statistic set approval_count = approval_count - 1 where user_id = ?1", nativeQuery = true)
     void decrementApprovalCountByUserId(Long userId);
 }
