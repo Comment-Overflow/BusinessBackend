cd9727342fc1b3ab1d323d94b137b3a27d1eb432
Xinpeng Wei <windowsxp@sjtu.edu.cn>
2021-09-01 23:43:39 +0800

    fix: remove filtering read poster out

--- src/main/java/com/privateboat/forum/backend/util/LogUtil.java
+++ src/main/java/com/privateboat/forum/backend/util/LogUtil.java
@@ -1,17 +1,20 @@
 package com.privateboat.forum.backend.util;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class LogUtil {
     public static void debug(Object e){
         String fullClassName = Thread.currentThread().getStackTrace()[2].getClassName();
         String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
         int lineNumber = Thread.currentThread().getStackTrace()[2].getLineNumber();
-        System.out.println("DEBUG\t" + fullClassName + "." + methodName + "():" + lineNumber + "\t" + e.toString());
+        log.info("DEBUG\t" + fullClassName + "." + methodName + "():" + lineNumber + "\t" + e.toString());
     }
 
     public static void error(Object e) {
         String fullClassName = Thread.currentThread().getStackTrace()[2].getClassName();
         String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
         int lineNumber = Thread.currentThread().getStackTrace()[2].getLineNumber();
-        System.out.println("ERROR\t" + fullClassName + "." + methodName + "():" + lineNumber + "\t" + e.toString());
+        log.info("ERROR\t" + fullClassName + "." + methodName + "():" + lineNumber + "\t" + e.toString());
     }
 }
